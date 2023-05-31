e184a0f79ab2a84f0667f8a4847eb55da97db919
justinlee05 <justinlee05@naver.com>
2023-05-31 11:20:04 +0900

    feat: 카드 상세보기

--- src/renderer/components/cardview/card.tsx
+++ src/renderer/components/cardview/card.tsx
@@ -10,6 +10,8 @@ import {
   CardActions,
   Menu,
   MenuItem,
+  Modal,
+  Box,
 } from '@mui/material';
 import { BukkitType } from 'atom/store';
 import { useState } from 'react';
@@ -38,61 +40,116 @@ export default function RecipeReviewCard(props: BukkitType) {
     setAnchorEl(null);
   };
 
+  const [modalOpen, setModalOpen] = useState(false);
+  const handleModalOpen = () => setModalOpen(true);
+  const handleModalClose = () => setModalOpen(false);
+
   return (
-    <Card sx={{ maxWidth: 345, width: 300 }}>
-      <CardHeader
-        avatar={
-          <Avatar aria-label="recipe">
-            <img src={image} alt="" />
-          </Avatar>
-        }
-        action={
-          <>
-            <IconButton aria-label="settings" onClick={handleClick}>
-              <MoreButton>
-                <img src={More} alt="" />
-              </MoreButton>
-            </IconButton>
-            <Menu
-              id="basic-menu"
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              MenuListProps={{
-                'aria-labelledby': 'basic-button',
-              }}
-            >
-              <MenuItem onClick={handleClose}>Edit</MenuItem>
-              <MenuItem onClick={handleClose}>Delete</MenuItem>
-              <MenuItem onClick={handleClose}>Complete</MenuItem>
-            </Menu>
-          </>
-        }
-        title={title}
-        subheader={`마감 기한 : ${target_date}`}
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          {content}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Members>
-          <p>참여자 명단</p>
-          <div>
-            {members.map((value) => (
-              <Avatar>
-                <img
-                  key={value.nickname}
-                  src={value.image}
-                  alt={value.nickname}
-                />
-              </Avatar>
-            ))}
-          </div>
-        </Members>
-      </CardActions>
-    </Card>
+    // 모달 추가할 것
+    <>
+      <Modal
+        open={modalOpen}
+        onClose={handleModalClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={Modalstyle}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            {title}
+          </Typography>
+          <Typography component="h3" sx={{ color: 'gray' }}>
+            {start_date} ~ {target_date}
+          </Typography>
+          <Typography
+            id="modal-modal-description"
+            sx={{ mt: 2, wordBreak: 'break-word' }}
+          >
+            {content}
+          </Typography>
+        </Box>
+      </Modal>
+      <Card
+        sx={{
+          maxWidth: 345,
+          width: 300,
+          cursor: 'pointer',
+        }}
+        onClick={() => {
+          handleModalOpen();
+        }}
+      >
+        <CardHeader
+          avatar={
+            <Avatar aria-label="recipe">
+              <img src={image} alt="" />
+            </Avatar>
+          }
+          action={
+            <>
+              <IconButton
+                aria-label="settings"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleClick(e);
+                }}
+              >
+                <MoreButton>
+                  <img src={More} alt="" />
+                </MoreButton>
+              </IconButton>
+              <Menu
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                  'aria-labelledby': 'basic-button',
+                }}
+              >
+                <MenuItem onClick={handleClose}>Edit</MenuItem>
+                <MenuItem onClick={handleClose}>Delete</MenuItem>
+                <MenuItem onClick={handleClose}>Complete</MenuItem>
+              </Menu>
+            </>
+          }
+          title={title}
+          subheader={`마감 기한 : ${target_date}`}
+        />
+        <CardContent>
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            sx={{
+              whiteSpace: 'nowrap',
+              textOverflow: 'ellipsis',
+              paddingRight: '10px',
+              overflow: 'hidden',
+            }}
+          >
+            {content}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Members>
+            <p>참여자 명단</p>
+            <div>
+              {members.map((value) => (
+                <Avatar>
+                  <img
+                    key={value.nickname}
+                    src={value.image}
+                    alt={value.nickname}
+                  />
+                </Avatar>
+              ))}
+            </div>
+          </Members>
+        </CardActions>
+      </Card>
+    </>
   );
 }
 
@@ -120,3 +177,15 @@ const MoreButton = styled.div`
   width: 20px;
   height: 20px;
 `;
+
+const Modalstyle = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '1px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
