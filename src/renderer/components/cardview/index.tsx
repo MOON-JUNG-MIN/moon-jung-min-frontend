6a823a9e5b3ee8914229de869711120860c97c84
paperdeer <tjrgus0713@gmail.com>
2023-06-15 13:43:59 +0900

    feat: 버킷 생성 버튼 추가

--- src/renderer/components/cardview/index.tsx
+++ src/renderer/components/cardview/index.tsx
@@ -1,15 +1,17 @@
 import { BucketItem } from 'atom/store';
 import styled from 'styled-components';
 import RecipeReviewCard from './card';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { instance } from 'renderer/apis/axios';
 import { useQuery } from 'react-query';
 import { Button } from '@mui/material';
+import { PlusIcon } from 'renderer/assets/PlusIcon';
 
 export default function CardView() {
   const { data } = useQuery(['getList'], () =>
     instance.get<{ bucket_list: BucketItem[] }>('/bucket')
   );
+  const navigate = useNavigate();
 
   return (
     <Wrapper
@@ -17,15 +19,6 @@ export default function CardView() {
         padding: '15px',
       }}
     >
-      <Button
-        variant="contained"
-        style={{
-          display: 'block',
-          marginLeft: 'auto',
-        }}
-      >
-        <Link to="/write">버킷 생성하기</Link>
-      </Button>
       <CardGrid>
         {data?.data.bucket_list.map((value) => (
           <RecipeReviewCard
@@ -42,6 +35,14 @@ export default function CardView() {
             room_name={value.room_name}
           />
         ))}
+        <CreateBucketButton
+          variant="outlined"
+          onClick={() => {
+            navigate('/write');
+          }}
+        >
+          <PlusIcon />
+        </CreateBucketButton>
       </CardGrid>
     </Wrapper>
   );
@@ -59,10 +60,11 @@ const CardGrid = styled.div`
   gap: 36px;
 `;
 
-const GoBack = styled.img`
-  border: 1px solid black;
-  padding: 10px;
-  margin: 5px;
-  background-color: cornsilk;
-  cursor: pointer;
+const CreateBucketButton = styled(Button)`
+  width: 100%;
+  height: 100%;
+  > svg {
+    width: 36px;
+    height: 36px;
+  }
 `;
